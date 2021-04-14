7760e769ba891473d1bd31dbe247f1e51531c38c
Liubomyr22 <70060037+Liubomyr22@users.noreply.github.com>
2021-04-14 17:37:18 +0300

    fix: react routing for gh pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { debounce } from "lodash";
 import { getMoviesList, getPopularMovies, getGenres } from "./api/api";
 import "./App.css";
